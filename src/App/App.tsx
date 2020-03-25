e22187b9fad401a19c44b664202770f6988a847a
Jacob <jakieschneider13@gmail.com>
2020-03-25 23:30:10 +1100

    Remove .idea (#2)
    * Build TypeScript React Project
    
    * Remove unnecessary files

---
+++ src/App/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default class App extends React.Component<any, any> {
+    render() {
+        return <div>
+            It's typescript and there's nothing you can do about it. <span aria-labelledby={"emojis"}>😆😆😆</span>
+        </div>;
+    }
+};

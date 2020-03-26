32c8bf8a060803025b2cd62789c2ba8b6a86b5cf
jbis9051 <josh9051@gmail.com>
2020-03-26 14:42:58 -0400

    Adds socket io. Adds socket io wrapper for event system management.

--- app.ts
+++ app.ts
@@ -11,4 +11,4 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-module.exports = app;
+export default app;
--- src/models/Socket.ts
+++ src/models/Socket.ts
@@ -1,5 +1,15 @@
-class Socket {
+import * as Events from 'events';
+
+interface Event {
+    event: string,
+    listener: (...args: any[]) => void
+}
+
+class Socket extends Events.EventEmitter {
     private _socket;
+    private sockets: Array<Object> = [];
+    private events: Array<Event> = [];
+    private allSockets = new Events.EventEmitter();
 
     get socket() {
         return this._socket;
@@ -7,6 +17,37 @@ class Socket {
 
     set socket(value) {
         this._socket = value;
+        this._socket.on("connection", (socket) => this.addSocket(socket));
+        this._socket.on("disconnect", (reason) => this.sockets.splice(this.sockets.indexOf(socket)));
+    }
+
+    constructor() {
+        super();
+        this.allSockets.on("newListener", (event, listener) => this.addListenerToSockets(event, listener));
+        this.allSockets.on("removeListener", (event) => this.removeListenerFromSockets(event));
+    }
+
+    addSocket(socket) {
+        this.sockets.push(socket);
+        this.events.forEach(event => {
+            socket.on(event.event, event.listener);
+        });
+    }
+
+    addListenerToSockets(event: string, listener: (...args: any[]) => void) {
+        this.socket.forEach(socket => {
+            socket.on(event, function () {
+                listener(socket, ...arguments);
+            });
+        });
+        this.events.push({event, listener});
+    }
+
+    removeListenerFromSockets(event: string) {
+        this.socket.forEach(socket => {
+            socket.removeListener(event);
+        });
+        this.events.splice(this.events.findIndex(event1 => event1.event === event), 1);
     }
 }
 

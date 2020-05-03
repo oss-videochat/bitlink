357e0ca8874eea4950852b9f61cd749f9bdaaaff
jbis9051 <josh9051@gmail.com>
2020-05-02 21:41:05 -0400

    AutoPlayAudio.tsx comments

--- src/components/Video/AutoPlayAudio.tsx
+++ src/components/Video/AutoPlayAudio.tsx
@@ -1,3 +1,15 @@
+/*
+
+This bypasses the autoplay block on iOS so we can autoplay audio when someone joins the room. HTML Video elements are
+allowed to be autoplayed as long as they are inline and muted. Audio elements can ONLY be played on user interaction or
+pragmatically after after user interaction.  What we do here is prepare 100 HTML Audio elements (`prepareAudioBank()`)
+by playing and pausing them on a user interaction event, specifically the click event of "Create Room" or "Join Room"
+buttons. Once they are prepared we can play them any time we want. The React component removes an audio element from the
+`audioBank` and uses it. Once it's done with it, it puts it back in the audioBank to be used again. This means we can
+technically only have 100 participant's at once (`Array.from({length: 100})`) but this can easily be increased. Ssshhh
+don't tell the nonexistent marketing team.
+ */
+
 import React from 'react';
 
 const audioBank: HTMLAudioElement[] = Array.from({length: 100}, el => document.createElement('audio') as unknown as HTMLAudioElement);
@@ -9,15 +21,21 @@ export function prepareAudioBank() {
     }
 
     audioBank.forEach((audioElement: HTMLAudioElement) => {
-        audioElement.play();
-        audioElement.pause()
+        try {
+            audioElement.play().catch(() => {
+                // this will defiantly error, but that's ok
+            });
+            audioElement.pause()
+        } catch (e) {
+            console.error("uh oh")
+        }
     });
 
     prepared = true;
 }
 
 export class AutoPlayAudio extends React.Component<any, any> {
-    private audioElement: HTMLAudioElement = audioBank.pop()!;
+    private audioElement: HTMLAudioElement = audioBank.pop() || document.createElement("audio"); // if the bank runs out, we just create another audio element. This will work for non-mobile/non-ios devices. But for those devices, we kind of screw them over here.
 
     constructor(props: any) {
         super(props);

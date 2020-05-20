307f7d4605b5081a1082316eda4a3b04e4a54280
jbis9051 <josh9051@gmail.com>
2020-05-20 14:53:18 -0400

    Adds better placeholder

--- src/components/Video/VideoPlaceholder.tsx
+++ src/components/Video/VideoPlaceholder.tsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import './VideoPlaceholder.css'
+import {RoomId} from "../Header/RoomId";
+import RoomStore from "../../stores/RoomStore";
 
 export class VideoPlaceholder extends React.Component<any, any> {
-    constructor(props: any) {
-        super(props);
-    }
-
     render() {
         return (
             <div className={"video-placeholder"}>
                 <span className={"video-placeholder--message"}>
-                    Invite Others
+                      Click the button to invite others.
+                    {RoomStore.room ?
+                        <RoomId/>
+                        : null
+                    }
                 </span>
             </div>
         );

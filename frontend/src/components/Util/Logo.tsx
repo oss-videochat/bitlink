9112db57f2c8a341bdd04272cd7102120a4e083f
jbis9051 <josh9051@gmail.com>
2020-08-10 18:06:51 -0400

    Add unstable warning

--- frontend/src/components/Util/Logo.tsx
+++ frontend/src/components/Util/Logo.tsx
@@ -3,13 +3,13 @@ import './Logo.css';
 import logo from "../../assets/logo/logo.svg";
 
 export const Logo: React.FunctionComponent = () => {
-    const version = "0.4.0";
-    const isBeta = version.startsWith("0");
+    const version = "1.0.0";
 
     return (
         <>
             <img className={"logo"} src={logo}/>
-            <span className={"version"}>v{version + (isBeta ? " beta" : "")}</span>
+            <span className={"version"}>v{version + " alpha (prerelease)"}</span>
+            <b className={"version"}>{"Unstable!"}</b>
         </>
     );
 }
